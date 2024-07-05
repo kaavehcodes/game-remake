6bcfdc613b09fb28870147bc24edd61c8edd548a
kaavehcodes <kaaveh.rostami@gmail.com>
2024-07-05 22:49:48 +0330

    Add canvas

--- javascript/pong/src/main.ts
+++ javascript/pong/src/main.ts
@@ -1,24 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let canvas: HTMLElement | null;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+window.onload = function () {
+  canvas = document.getElementById("game");
+
+  const ctx = canvas?.getContext("2d");
+
+  ctx?.fillStyle;
+  ctx?.fillRect(0, 0, canvas?.width, canvas?.height);
+
+}
