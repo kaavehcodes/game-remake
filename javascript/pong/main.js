91c05ecdd9ac0af55d954751670962bf7ea6b2a2
kaavehcodes <kaaveh.rostami@gmail.com>
2024-07-31 22:05:44 +0330

    Add canvas to pong

--- javascript/pong/main.js
+++ javascript/pong/main.js
@@ -5,5 +5,20 @@ let canvas;
 let ctx;
 
 window.onload = function () {
-  console.log("Hello World!");
+  canvas = document.getElementById("pong");
+  canvas.width = 800;
+  canvas.height = 600;
+
+  ctx = canvas.getContext("2d");
+
+  render();
+}
+
+function render() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function update() {
+  console.log("Update");
 }
