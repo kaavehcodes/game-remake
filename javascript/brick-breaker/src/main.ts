af4ee27bcfb8125e218aa27f00ebdaa237c0d85b
kaavehcodes <kaaveh.rostami@gmail.com>
2024-08-07 16:41:05 +0330

    Setup brick breaker game

---
+++ javascript/brick-breaker/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
+window.onload = function () {
+  load();
+}
+
+function load() {
+  console.log("Game loaded!");
+}
